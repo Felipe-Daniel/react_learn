e297838de8f9f9b63ab3f5d5afe303c467aef828
Felipe-Daniel <felipedanielobmep2014@yahoo.com.br>
2021-10-12 14:23:47 -0300

    Added portals, ref's, and fragments to the code

--- src/Components/AddUser/AddUser.js
+++ src/Components/AddUser/AddUser.js
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import React, { useState, useRef } from "react";
+import ReactDOM from "react-dom";
 import styles from "./AddUser.module.scss";
 import Modal from "./Modal";
 
 function AddUser(props) {
-  const [userName, setUserName] = useState("");
-  const [age, setAge] = useState("");
+  const usernameRef = useRef("");
+  const ageRef = useRef("");
 
   const [modal, setModal] = useState();
 
@@ -14,44 +15,53 @@ function AddUser(props) {
 
   function submit(event) {
     event.preventDefault();
-    if (userName.length === 0) {
+    if (usernameRef.current.value.length === 0) {
       setModal("Please enter a username");
-    } else if (isNaN(Number(age)) || age < 0) {
+    } else if (
+      isNaN(Number(ageRef.current.value)) ||
+      ageRef.current.value < 0 ||
+      ageRef.current.value === ""
+    ) {
       setModal('Please enter a valid age ( {">"}0)!');
     } else {
       const data = {
-        key: userName,
-        userName: userName,
-        age: age,
+        key: usernameRef.current.value,
+        userName: usernameRef.current.value,
+        age: ageRef.current.value,
       };
       props.onLiftInputData(data);
+      usernameRef.current.value = "";
+      ageRef.current.value = "";
     }
   }
   return (
-    <div className={styles["form-container"]}>
+    <React.Fragment>
       <form onSubmit={submit}>
         <div className={styles["form-block"]}>
           <label className={styles["form-block__label"]}>Username</label>
           <input
             className={styles["form-block__input"]}
-            onChange={(event) => setUserName(event.target.value)}
+            ref={usernameRef}
           ></input>
         </div>
         <div className={styles["form-block"]}>
           <label className={styles["form-block__label"]}>Age (Years)</label>
-          <input
-            className={styles["form-block__input"]}
-            onChange={(event) => setAge(event.target.value)}
-          ></input>
+          <input className={styles["form-block__input"]} ref={ageRef}></input>
         </div>
         <button type="submit" className={styles["submit-button"]}>
           Add User
         </button>
       </form>
-      {modal && (
-        <Modal isOpen={modal} onLiftModal={liftModalHandler} message={modal} />
-      )}
-    </div>
+      {modal &&
+        ReactDOM.createPortal(
+          <Modal
+            isOpen={modal}
+            onLiftModal={liftModalHandler}
+            message={modal}
+          />,
+          document.getElementById("modal")
+        )}
+    </React.Fragment>
   );
 }
 export default AddUser;
